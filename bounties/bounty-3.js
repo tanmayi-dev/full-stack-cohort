d916107246a712af896bbabbf1969331c7b8fbe9
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2023-12-23 19:19:43 +0530

    week2 notes

--- bounties/bounty-3.js
+++ bounties/bounty-3.js
@@ -1,1 +1,52 @@
 // Create your own Promise class
+
+/**
+
+class CustomPromise {
+  resolveHandler;
+  rejectHandler;
+  isCompleted = false;
+
+  constructor(executor) {
+    this.then = (onResolve) => {
+      this.resolveHandler = onResolve;
+      return this;
+    };
+
+    this.catch = (onReject) => {
+      this.rejectHandler = onReject;
+      return this;
+    };
+
+    const resolve = (value) => {
+      this.executeHandler(this.resolveHandler, value);
+    };
+
+    const reject = (reason) => {
+      this.executeHandler(this.rejectHandler, reason);
+    };
+
+    executor(resolve, reject);
+  }
+
+  executeHandler(handler, value) {
+    if (this.isFunction(handler) && !this.isCompleted) {
+      handler(value);
+      this.isCompleted = true;
+    }
+  }
+
+  isFunction(checkFunction) {
+    return (
+      checkFunction && {}.toString.call(checkFunction) === "[object Function]"
+    );
+  }
+}
+
+// Evaluate CustomPromise
+const sample = new CustomPromise((resolve, reject) => {
+  setTimeout(() => resolve(1), 5000);
+});
+
+sample.then((val) => console.log(`Resolved value: ${val}`));
+ */
---
+++ bounties/bounty-4.js
@@ -0,0 +1,5 @@
+// Create a HTTP Server from scratch in C/C++
+// Create an http server in rust using actix-web
+// Create an http server in golang using the gorilla framework
+
+// Create a Todo application where you store data in a file
